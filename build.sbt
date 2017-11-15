98d7a41db2178eba1a48dc207fb14a88f2a85fe4
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-11-15 18:11:53 -0300

    Created the main function, for now just spits out the file's content.

--- build.sbt
+++ build.sbt
@@ -9,7 +9,8 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.0.4" % "test",
   "org.apache.poi" % "poi" % "3.15-beta2",
   "org.apache.poi" % "poi-ooxml" % "3.15-beta2",
-  "org.apache.poi" % "poi-ooxml-schemas" % "3.15-beta2"
+  "org.apache.poi" % "poi-ooxml-schemas" % "3.15-beta2",
+  "org.apache.poi" % "poi-scratchpad" % "3.15-beta2"
 )
 
 mainClass := Some("com.marcastr0.DocxFindAndReplace")
---
+++ src/main/scala/com/marcastr0/DocxFindAndReplace.scala
@@ -0,0 +1,20 @@
+package com.marcastr0
+
+import java.io.{FileInputStream, InputStream}
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument
+
+object DocxFindAndReplace {
+  def main(args: Array[String]): Unit = {
+    if (args.length == 0) {
+      println("Must receive a Word file as input.")
+    } else {
+      val docxFile = args(0)
+      val fileStream: InputStream = new FileInputStream(docxFile)
+      val document = new XWPFDocument(fileStream)
+
+      val text = document.getDocument
+      println(text)
+    }
+  }
+}
