8ebb231ec344a5fe2c0e9ec1f7db64acc2e6c4f2
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-11-16 14:55:47 -0300

    Added a find by regex function.

--- src/main/scala/com/marcastr0/DocxReader.scala
+++ src/main/scala/com/marcastr0/DocxReader.scala
@@ -3,10 +3,21 @@ package com.marcastr0
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor
 import org.apache.poi.xwpf.usermodel.XWPFDocument
 
+import scala.util.matching.Regex
+
 object DocxReader {
   def findString(document: XWPFDocument, string: String): Boolean = {
     val extractor = new XWPFWordExtractor(document)
     val text = extractor.getText
     text contains string
+  }
+
+  def findRegex(document: XWPFDocument, regex: String): Boolean = {
+    val pattern = new Regex(regex)
+    val extractor = new XWPFWordExtractor(document)
+    val text = extractor.getText
+    val mtch = pattern.findFirstIn(text)
+    println(mtch.getOrElse(""))
+    mtch.getOrElse(false) match {case _ => true}
   }
 }
--- src/test/scala/com/marcastr0/DocxReaderSpec.scala
+++ src/test/scala/com/marcastr0/DocxReaderSpec.scala
@@ -6,13 +6,21 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument
 import org.scalatest.FlatSpec
 
 class DocxReaderSpec extends FlatSpec {
-  "findString" should "find whether a text contains a string (without using regex)" in {
-    val docxFile: InputStream = getClass.getResourceAsStream("/helloworld.docx")
-    val document = new XWPFDocument(docxFile)
+  val docxFile: InputStream = getClass.getResourceAsStream("/helloworld.docx")
+  val document = new XWPFDocument(docxFile)
 
+  "findString" should "find whether a document contains a string (without using regex)" in {
     assert(DocxReader.findString(document, "Hello World"))
     assert(!DocxReader.findString(document, "Hello Wrld"))
     assert(DocxReader.findString(document, "Lorem ipsum"))
     assert(!DocxReader.findString(document, "lorem ipsum"))
+  }
+
+  "findRegex" should "find whether a document contains a string that matches the given regular expression" in {
+    assert(DocxReader.findRegex(document, "^Hello World!$"))
+    assert(DocxReader.findRegex(document, "^Hello \\w+!$"))
+    assert(DocxReader.findRegex(document, "Lorem .+, consectetur"))
+    // assert(!DocxReader.findRegex(document, "Lorem \\w+[,]{1} dolor"))
+    // assert(!DocxReader.findRegex(document, "Lorem .+, adipiscing"))
   }
 }
